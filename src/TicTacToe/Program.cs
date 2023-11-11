fda42151854c452c09f4099f7c59931a0faffbd7
David Chen <davidchen@d-techpro.net>
2023-11-11 06:08:41 -0500

    Added MSSQL Clustering

--- src/TicTacToe/Program.cs
+++ src/TicTacToe/Program.cs
@@ -1,25 +1,71 @@
-using System.Net;
 
-await Host.CreateDefaultBuilder(args)
-    .UseOrleans((ctx, siloBuilder) =>
+using Orleans.Configuration;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true, true)
+    .AddEnvironmentVariables();
+
+var config = builder.Configuration.GetSection("AzureKayVault");
+var dnsNameKeyVault = config["DNSNameKeyVault"];
+
+if (!string.IsNullOrWhiteSpace(dnsNameKeyVault))
+{
+    builder.Configuration.AddAzureKeyVault($"{dnsNameKeyVault}",
+        config["AADAppRegistrationAppId"],
+        config["AADAppRegistrationAppSecret"]);
+}
+
+var keyConfigName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production" ? "" : "-dev";
+
+var sqlConnStr = builder.Configuration[$"mssqlorleans{keyConfigName}"];
+
+builder.Host.UseOrleans((ctx, siloBuilder) =>
+{
+    siloBuilder.AddAdoNetGrainStorage("OrleansStorage", options =>
     {
-        // In order to support multiple hosts forming a cluster, they must listen on different ports.
-        // Use the --InstanceId X option to launch subsequent hosts.
-        var instanceId = ctx.Configuration.GetValue<int>("InstanceId");
-        var port = 11_111;
-        siloBuilder.UseLocalhostClustering(
-            siloPort: port + instanceId,
-            gatewayPort: 30000 + instanceId,
-            primarySiloEndpoint: new IPEndPoint(IPAddress.Loopback, port));
+        options.Invariant = "System.Data.SqlClient";
+        options.ConnectionString = sqlConnStr;
     })
-    .ConfigureWebHostDefaults(webBuilder =>
+    .Configure<ClusterOptions>(options =>
     {
-        webBuilder.UseStartup<Startup>();
-        webBuilder.ConfigureKestrel((ctx, kestrelOptions) =>
-        {
-            // To avoid port conflicts, each Web server must listen on a different port.
-            var instanceId = ctx.Configuration.GetValue<int>("InstanceId");
-            kestrelOptions.ListenLocalhost(8080 + instanceId);
-        });
+        options.ClusterId = "ClusterTicTacToe";
+        options.ServiceId = "AwesomeTicTacToeService";
     })
-    .RunConsoleAsync();
+    .UseAdoNetClustering(options =>
+    {
+        options.ConnectionString = sqlConnStr;
+        options.Invariant = "System.Data.SqlClient";
+    })
+        .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+        .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Warning).AddConsole());
+});
+
+builder.WebHost.ConfigureKestrel((ctx, kestrelOptions) =>
+{
+    // To avoid port conflicts, each Web server must listen on a different port.
+    var instanceId = ctx.Configuration.GetValue<int>("InstanceId");
+    kestrelOptions.ListenLocalhost(8080 + instanceId);
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
